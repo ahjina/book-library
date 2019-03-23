eafb16fb99f126de4a24cc456f5d6f48c03ab06e
Kuro Ryuu <phuong030616@gmail.com>
2019-03-23 14:59:38 +0700

    Init project
    Create angular project

---
+++ project/src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AppRoutingModule } from './app.routing';
+
+@NgModule({
+  declarations: [AppComponent, DashboardComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}

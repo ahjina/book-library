f41bdcde9082cfbb0d4203624484c2d72382a22a
Kuro Ryuu <littledragonlovehoney@gmail.com>
2019-04-06 22:52:08 +0700

    Login page
    + create login page
    + add background image for login page

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AppRoutingModule } from './app.routing';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [AppComponent, DashboardComponent],
+  declarations: [AppComponent, DashboardComponent, LoginComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
--- project/src/app/app.routing.ts
+++ project/src/app/app.routing.ts
@@ -2,12 +2,21 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {
     path: 'home',
     component: DashboardComponent
-  }
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
 ];
 
 @NgModule({
